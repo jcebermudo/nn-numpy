7ea4bc72b4aa33a356464fbb14d1618620bb3709
James <jamestech718@gmail.com>
2026-05-20 11:04:18 +0800

    update imports

--- src/mnist.py
+++ src/mnist.py
@@ -1,10 +1,9 @@
-import matplotlib
-import matplotlib.pyplot as plt
-from sklearn.datasets import fetch_mldata
+from sklearn.datasets import fetch_openml
 import numpy as np
 
-mnist = fetch_mldata('MNIST original')
+mnist = fetch_openml("mnist_784", version=1, as_frame=False, parser="liac-arff")
 X, y = mnist["data"], mnist["target"]
+y = y.astype(float)
 
 X = X / 255
 
@@ -23,8 +22,5 @@ shuffle_index = np.random.permutation(m)
 X_train, y_train = X_train[:,shuffle_index], y_train[:,shuffle_index]
 
 
-i = 3
-plt.imshow(X_train[:,i].reshape(28,28), cmap = matplotlib.cm.binary)
-plt.axis("off")
-plt.show()
-print(y_train[:,i])
+
+
